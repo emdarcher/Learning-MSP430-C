df82952fb98865a0dda8293c6427dfdeb9933a2d
Ethan Durrant <emd519@yahoo.com>
2014-05-26 20:32:54 -0700

    functions done, now test

--- projects/readInternalTemp/readInternalTemp.c
+++ projects/readInternalTemp/readInternalTemp.c
@@ -42,11 +42,18 @@ pull segment's pin LOW to turn it on
 #define SEG_F BIT2
 #define SEG_G BIT1
 
+#define ALL_SEGS ( SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G )
+
 #define DIG_0 BIT0
 #define DIG_1 BIT1
 
+//remember to add any newly defines digits here
+#define ALL_DIGS ( DIG_0 | DIG_1 )
+
 //prototypes
-void write_number_digit(unsigned char num, unsigned char dig);
+void clear_display(void);
+void write_digit(unsigned char num, unsigned char dig);
+void write_number(unsigned char number);
 
 //add any defined digits to this array
 unsigned char digit_bits[] = { DIG_0, DIG_1 };
@@ -72,15 +79,49 @@ unsigned char number_seg_bytes[] = {
 
 void main(void)
 {
+	
 	WDTCTL = WDTPW + WDTHOLD; //disable watchdog
 
 	//setup port 1 registers?
-	P1DIR = (SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G);
+	P1DIR |= ALL_SEGS;
 	
 	//sets of digits
-	for (i=0; i < sizeof(digit_bits); i++){
-		P2DIR |= digit_bits[i];		
-	}
+	P2DIR |= ALL_DIGS;
+	//for (i=0; i < sizeof(digit_bits); i++){
+	//	P2DIR |= digit_bits[i];		
+	//}	P2OUT &= ~(ALL_DIGS); //set digits LOW so nothing goes on	
 	
+	clear_dispay();
+
+	for(;;){
+		//test
+		write_number(18);		
+	}
+
+	return 0; //should never reach this point
+}
+
+//function to clear everything
+void clear_display(void){
+	P2OUT &= ~(ALL_DIGS); //set digits LOW so nothing goes on       
+
+
+        P1OUT |= ALL_SEGS; //set all segs off by putting them high;
+
+}
+
+void write_digit(unsigned char num, unsigned char dig){
+	
+	clear_display();
+	P2OUT |= digit_bits[dig];
+	P1OUT = number_seg_bytes[num]; 
+
+}
+
+void write_number(unsigned char number){
+
+	//formats number based on digits to correct digits on display
+	write_digit((number%10), 0 );
+	write_digit((number/10), 1 );		
 
 }
