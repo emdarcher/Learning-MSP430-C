14496ab3104f2e4bf7cac0bcff0b8c5198d00685
Ethan Durrant <emd519@yahoo.com>
2014-05-26 19:03:11 -0700

    things started, related to internal temp sensor and 7 segmets

---
+++ projects/readInternalTemp/readInternalTemp.c
@@ -0,0 +1,23 @@
+/* program to read the internal temp sensor 
+and show it on a 2 digit 7-segment display */
+
+
+/*
+segments
+ ---A---
+|       |
+F       B
+|___G___|
+|       |
+E       C
+|___D___|
+
+*/
+/*
+
+pinout of display
+
+C1
+
+
+*/
