be3e52dcb5ac5be948b13bebe696c8a6ae51a229
Ethan Durrant <emd519@yahoo.com>
2014-05-26 20:08:52 -0700

    added bytes for numbers port 1 stuff

--- projects/readInternalTemp/readInternalTemp.c
+++ projects/readInternalTemp/readInternalTemp.c
@@ -34,21 +34,41 @@ pull segment's pin LOW to turn it on
 #include <msp430.h>
 
 //defines
-#define SEG_A BIT0
-#define SEG_B BIT1
-#define SEG_C BIT2
-#define SEG_D BIT3
-#define SEG_E BIT4
-#define SEG_F BIT5
-#define SEG_G BIT7
+#define SEG_A BIT7
+#define SEG_B BIT6
+#define SEG_C BIT5
+#define SEG_D BIT4
+#define SEG_E BIT3
+#define SEG_F BIT2
+#define SEG_G BIT1
 
 #define DIG_0 BIT0
 #define DIG_1 BIT1
 
+//prototypes
+void write_number_digit(unsigned char num, unsigned char dig);
+
 //add any defined digits to this array
 unsigned char digit_bits[] = { DIG_0, DIG_1 };
-
+//int i used for "for" loops
 int i;
+
+//array to store bytes of port 1 values to make numbers
+unsigned char number_seg_bytes[] = {
+//       unconfigured
+//ABCDEFG^
+0b00000010;//0
+0b10011110;//1
+0b00100100;//2
+0b00001100;//3
+0b10011000;//4
+0b01001000;//5
+0b01000000;//6
+0b00011110;//7
+0b00000000;//8
+0b00011000;//9
+
+};
 
 void main(void)
 {
@@ -61,5 +81,6 @@ void main(void)
 	for (i=0; i < sizeof(digit_bits); i++){
 		P2DIR |= digit_bits[i];		
 	}
+	
 
 }
