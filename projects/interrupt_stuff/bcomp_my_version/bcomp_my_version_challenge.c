fad7fce47cdc9ce5d5879a2f90304b909f71ea65
Ethan Durrant <emd519@yahoo.com>
2014-05-29 15:30:22 -0700

    exercise challenge now finished, but there is probably a bit better way to do it

--- projects/interrupt_stuff/bcomp_my_version/bcomp_my_version_challenge.c
+++ projects/interrupt_stuff/bcomp_my_version/bcomp_my_version_challenge.c
@@ -16,10 +16,18 @@
 
 #define AIN1 BIT1
 
+#define REF_SWITCH_DELAY 12 //number of cycles to delay between
+								//comparator ref switching
+
 //	Global Variables
 char flash = 0;					// start with no flash on LED
 
+volatile unsigned char current_led, other_led;
+volatile unsigned char comp_state;
+
 //	Function Prototypes/Definitions
+
+void comp_ref_to_led(void);
 
 void main(void) {
 	WDTCTL = WDTPW + WDTHOLD; //disable watchdog
@@ -29,14 +37,29 @@ void main(void) {
 	P1OUT = 0;
 	P1DIR |= (LED1|LED2);				// output on P1.0 for LED1
 										// and P1.6 for LED2
+	
+	current_led = LED1; //start with LED1 b/c it initializes with 0.5 Vcc
+	comp_state = 1;					
+						//clarification!, took me forever to figure out.
+						//CAREF1 ---- CAREF0
+						//bit5			bit4
+						//look at datasheet for better details
 						
 	CACTL1 = CAREF1 + CARSEL + CAIE;	// 0.5 Vcc ref on - pin, enable
 										// interrupts on rising edge.
+	//CACTL1 &= CAREF0;
+	
 	CACTL2 = P2CA4 + CAF;		// Input CA1 on + pin, filter output.
 	CAPD = AIN1;				// disable digital I/O on P1.1 (technically
 								// this step is redundant)
 	
-	TACCR0 = 60000;				// Timer delay for LED flash, 60000 cycles
+	TACCR0 = REF_SWITCH_DELAY;				// Timer delay for comparator
+											//reference switching
+											// With the Timer using SMCLK div 8 (125 kHz), this
+							// value gives a frequency of 125000/(TACCR0+1) Hz.
+							// For TACCR0 = 144, that's 862 Hz.
+							// at 14400 it is 8.7... Hz
+							
 	TACCTL0 = CCIE;     		// Enable interrupts for CCR0.
 	TACTL = TASSEL_2 + ID_3 + MC_1 + TACLR;  // SMCLK, div 8, up mode, 
 	                                         // clear timer
@@ -54,32 +77,54 @@ void main(void) {
 
 //	Functions
 
+void comp_ref_to_led(void){
+
+	//if ((CACTL1 & CAREF1) == 0x01){ //if CAREF1 is 1, so 0.5 Vcc
+	if (comp_state){
+		current_led = LED1; //if higher that 0.5 Vcc is satisfies
+										//both conditions
+		other_led = LED2;
+	} else {
+		current_led = LED2;
+		other_led = 0;
+	}
+}
+
 //	Interrupt Service Routines
 
 __attribute__((interrupt(TIMER0_A0_VECTOR))) //notice! for the 20pin chips:
 											//had to change TIMERA0_VECTOR
 											//to TIMER0_A0_VECTOR 
 void CCR0_ISR(void){
-	//P1OUT ^= flash;// if flash is zero, keep LED off
-								// if flash is LED1, toggle LED.
-	if (flash >0){
-		P1OUT |= flash;
+	CACTL1 &= ~CAON; //turn off comparator for settings change
+	
+	//toggle CAREF1 to set between 0.5 to 0.25 Vcc
+	CACTL1 ^= CAREF1;
+	//toggle CAREF0 as well
+	CACTL1 ^= CAREF0;
+	/*
+	if ((CACTL1 & CAREF1) == 0x01){ //if CAREF1 is 1, so 0.5 Vcc
+		current_led = LED1;
 	} else {
-		P1OUR &= flash;
-	}
+		current_led = LED2;
+	} 
+	*/
+	comp_state ^= 1;
+	CACTL1 |= CAON; //turn back on
 }
 
 __attribute__((interrupt(COMPARATORA_VECTOR)))
 void COMPA_ISR(void) {
+	
+	comp_ref_to_led();
 		
-		if ((CACTL2 & CAOUT)==0x01) {
+	if ((CACTL2 & CAOUT)==0x01) {
 		CACTL1 |= CAIES;		// value high, so watch for falling edge
-		flash = LED1;			// let LED flash
+		P1OUT |= current_led + other_led;			
 	}
 	else {
 		CACTL1 &= ~CAIES;		// value low, so watch for rising edge
-		flash = 0;				// turn LED off
-		P1OUT = 0;
+		P1OUT &= ~(current_led);
 	}	
 		
 }
